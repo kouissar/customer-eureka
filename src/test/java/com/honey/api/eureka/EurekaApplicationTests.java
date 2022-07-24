2d5470dcdfdafd81f2e761ac4e8db957d22fbc38
kouissar <kouissar@gmail.com>
2022-07-24 12:02:46 -0400

    Initial commit

---
+++ src/test/java/com/honey/api/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.honey.api.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
